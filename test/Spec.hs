f7406f7e84ba28d2d148793fdf193727063d368d
joe9 <joe9mail@gmail.com>
2016-10-26 05:59:11 -0700

    failed attempt at capturing stdout

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,9 +2,12 @@
 
 module Main where
 
+import Control.Exception.Safe
 import Network.Simple.TCP
 import Protolude
-import Test.Tasty         (defaultMain, testGroup)
+-- import System.IO.Silently
+import System.IO.Capture
+import Test.Tasty             (defaultMain, testGroup)
 
 import Network.NineP
 
@@ -12,19 +15,29 @@ import qualified Response.Tests
 import           Server.Tests
 
 -- import qualified Keymap.CustomDvorak.Tests
+-- https://github.com/hspec/silently
 main :: IO ()
 main = do
-  let context = testContext
   withAsync
-    (run9PServer context (Host "127.0.0.1") "5961")
-    (\_ -> do
-       threadDelay 1000
-       connect
-         "127.0.0.1"
-         "5961"
-         (\(connectionSocket, remoteAddr) -> do
-            putStrLn $ "Connection established to " ++ show remoteAddr
-            defaultMain
-              (testGroup
-                 "Tests"
-                 [Response.Tests.tests, Server.Tests.tests connectionSocket])))
+     silent9PServer
+     (\_ -> do
+        threadDelay 1000
+        connect
+            "127.0.0.1"
+            "5961"
+            (\(connectionSocket, remoteAddr) -> do
+                putStrLn ( "Connection established to " ++ show remoteAddr)
+                defaultMain
+                    (testGroup
+                        "Tests"
+                        [Response.Tests.tests, Server.Tests.tests connectionSocket])))
+
+silent9PServer :: IO ()
+silent9PServer = do
+  let context = testContext
+  (captured, _,_,_) <-
+    capture
+        (catchAny -- catch the "cancel" sent by the tests thread
+            (run9PServer context (Host "127.0.0.1") "5961")
+                (\e -> do putStrLn ("Got an exception: " ++ show e)))
+  putStr "captured: " >> putStrLn captured
